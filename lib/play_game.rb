87efe7269186d611334922f3e2f90937835d7435
Victor Elizalde <vic-bull@hotmail.com>
2021-03-04 02:07:39 -0600

    Initializes new classes

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -5,8 +5,12 @@ require 'computer'
 require 'human'
 require 'referee'
 require 'ui'
+require 'terminal_input'
+require 'browser_input'
+require 'curler'
+require 'file_handler'
 
 referee = Referee.new
 
-current_game = Game.new(Board.new, UI.new, referee, [Human.new('X'), Computer.new('O', referee)])
+current_game = Game.new(Board.new, UI.new, referee, [Human.new('X', TerminalInput.new, BrowserInput.new), Computer.new('O', referee)], nil, Curler.new, FileHandler.new)
 current_game.play_game
